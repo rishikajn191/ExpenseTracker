e71b794547cfd3775cca7e9d41db33e5747038a0
Rishika Jain <rishika.jn191@gmail.com>
2025-07-17 15:08:21 +0530

    🔧 Restore and update UI, charts, and deployment setup

---
+++ public/firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.template.js
+export const firebaseConfig = {
+  apiKey: "${FIREBASE_API_KEY}",
+  authDomain: "${FIREBASE_AUTH_DOMAIN}",
+  projectId: "${FIREBASE_PROJECT_ID}",
+  storageBucket: "${FIREBASE_STORAGE_BUCKET}",
+  messagingSenderId: "${FIREBASE_MESSAGING_SENDER_ID}",
+  appId: "${FIREBASE_APP_ID}",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
