e39aee2f7e0817df086f14f054e429d0055ae760
Rishika Jain <rishika.jn191@gmail.com>
2025-07-18 17:22:00 +0530

    Update firebase-config.template.js

--- public/firebase-config.template.js
+++ public/firebase-config.template.js
@@ -6,5 +6,3 @@ const firebaseConfig = {
   messagingSenderId: "${FIREBASE_MESSAGING_SENDER_ID}",
   appId: "${FIREBASE_APP_ID}",
 };
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
